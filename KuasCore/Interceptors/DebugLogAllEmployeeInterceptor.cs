2427d18a1e4b4b96d0d418537de13a0737e610fa
1101137154 <mahendraagnidharma@yahoo.co.id>
2015-04-17 17:19:06 +0800

    first commit

---
+++ KuasCore/Interceptors/DebugLogAllEmployeeInterceptor.cs
@@ -0,0 +1,29 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace KuasCore.Interceptors 
+{
+    class DebugLogAllEmployeeInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            IList<Employee> result = (IList<Employee>)invocation.Proceed();
+
+            foreach(Employee x in result){
+                x.Name = "secret";
+            }
+
+            Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            Debug.Print("This Is the Result [{0}]", result);
+
+            return result;
+        }
+    }
+}
---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Diagnostics;
+
+namespace KuasCore.Interceptors 
+{
+    public class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            Employee result = (Employee)invocation.Proceed();
+
+            result.Name += " You access the db!";
+
+            Console.WriteLine("AAAA回傳的資料已取得 [{0}]", result);
+            Debug.Print("SSSS回傳的資料已取得 [{0}]", result);
+
+            return result;
+        }
+    }
+}
